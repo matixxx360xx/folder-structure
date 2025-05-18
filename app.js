78c85c38a09e111c54c57088890bd75d4a77db1e
MSI\mati <matimichalski01@gmail.com>
2025-05-18 21:35:22 +0200

    Dodano metode dodajPliki co dadaje pliki do folderu

--- app.js
+++ app.js
@@ -3,8 +3,28 @@ const path = require("path");
 const prompt = require('prompt-sync')();
 
 class Folder{
-dodajfoler(){
-    fs.mkdirSync('nowy-folder');
+    constructor(){
+        this.nazwaFolderu = '';
+    }
+dodajFolder(){
+    this.nazwaFolderu = prompt('Dodaj nazwe folderu: ');
+    try{
+    fs.mkdirSync(`${this.nazwaFolderu}`);
+    }catch(err){
+        console.log("Błąd podczas tworzenia folderu");
+    }
+}
+
+dodajPliki(){
+    const ilePlikow = parseInt(prompt('Ile plików chcesz utworzyć?'));
+    for(let i = 0; i < ilePlikow; i++){
+        let nazwaPliku = prompt('Dodaj nazwe pliku: ');
+        try{
+        fs.writeFileSync(path.join(this.nazwaFolderu, nazwaPliku), '');
+        }catch(err){
+            console.log("Błąd podczas tworzenia pliku");
+        }
+    }
 }
 }
 
@@ -12,18 +32,22 @@ function main(){
 const folder = new Folder();
 
 
-
-
-
 while(true){
-    console.log("1. Dodaj folder\n2. Wyjscie");
+    console.log("1. Dodaj folder \n2. Dodaj pliki \n3. Wyjscie");
 
     let option = prompt('Wybierz opcje: ');
     switch(option){
         case '1':
-            folder.dodajfoler();
+            folder.dodajFolder();
+            break;
         case '2':
+            folder.dodajPliki();
+            break;
+        case '3':
             return;
+        default:
+            console.log("wybierz opcje od 1 do 3");
+            continue;
     }
 }
 }
